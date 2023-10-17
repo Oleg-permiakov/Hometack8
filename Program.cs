22e09e73826e9dc84952cf83cdc9e4ff1fab63d3
Oleg <schika145@gmail.com>
2023-10-17 14:46:06 +0500

    Конец

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задать число N и вывести все чётные числа до N
+Console.WriteLine("Введите число");
+string anamber = Console.ReadLine();
+int namber = Convert.ToInt32(anamber);
+
+int a = 2;
+while (a <= namber)
+{
+            Console.WriteLine(a);
+            a = a + 2;
+}
